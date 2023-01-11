a3b5abb9bb381e360e8f8cccf24fc17c547e218d
Wolfram3695 <nikitka.ilev@mail.ru>
2023-01-11 15:15:37 +0300

    123

--- lab5.js
+++ lab5.js
@@ -8,13 +8,27 @@ function Func1(){
 }
 
 function Func2(){
-
+    document.getElementById('information').style.display = 'block';
+    document.getElementById('new2').style.display = 'block';
+    document.getElementsByClassName("new")[0].style.opacity = '0.3';
+    document.getElementsByClassName("new")[1].style.opacity = '0.3';
+    document.getElementsByClassName("new")[2].style.opacity = '0.3';
 }
 
 function Func3(){
-
+    document.getElementById('information').style.display = 'block';
+    document.getElementById('new3').style.display = 'block';
+    document.getElementsByClassName("new")[0].style.opacity = '0.3';
+    document.getElementsByClassName("new")[1].style.opacity = '0.3';
+    document.getElementsByClassName("new")[2].style.opacity = '0.3';
 }
 
-function close(){
-    
+function closea(){
+    document.getElementById('information').style.display = 'none';
+    document.getElementById('new1').style.display = 'none';
+    document.getElementById('new2').style.display = 'none';
+    document.getElementById('new3').style.display = 'none';
+    document.getElementsByClassName("new")[0].style.opacity = '1';
+    document.getElementsByClassName("new")[1].style.opacity = '1';
+    document.getElementsByClassName("new")[2].style.opacity = '1';
 }
