4f8e70ad4c846a275af463b49c852e47f6a4b63b
Wolfram3695 <nikitka.ilev@mail.ru>
2023-01-11 14:56:46 +0300

    123

---
+++ lab5.js
@@ -0,0 +1,20 @@
+function Func1(){
+    document.getElementById('information').style.display = 'block';
+    document.getElementById('new1').style.display = 'block';
+    document.getElementsByClassName("new")[0].style.opacity = '0.3';
+    document.getElementsByClassName("new")[1].style.opacity = '0.3';
+    document.getElementsByClassName("new")[2].style.opacity = '0.3';
+
+}
+
+function Func2(){
+
+}
+
+function Func3(){
+
+}
+
+function close(){
+    
+}
